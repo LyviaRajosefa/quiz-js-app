55713ddfd66f62441092b6d52254e91dc621812e
Lyvia <lyviaraj@gmail.com>
2026-02-06 02:25:16 +0300

    ajout questions

--- question.js
+++ question.js
@@ -15,41 +15,41 @@ export const Questions = [
       answers: ['4', '"22"', '22', 'Erreur'],
       correct: '"22"',
     },
-    // {
-    //   question: "Comment appelle-t-on une fonction qui s'appelle elle-même ?",
-    //   answers: ['Fonction circulaire', 'Fonction récursive', 'Fonction miroir'],
-    //   correct: 'Fonction récursive',
-    // },
-    // {
-    //   question: 'Quelle fonction permet de créer un délai en JavaScript ?',
-    //   answers: ['delay()', 'setTimeout()', 'wait()'],
-    //   correct: 'setTimeout()',
-    // },
-    // {
-    //   question:
-    //     'Quel est le nom de la fonction qui permet de répéter une action à intervalles réguliers ?',
-    //   answers: ['setInterval()', 'repeat()', 'loop()'],
-    //   correct: 'setInterval()',
-    // },
-    // {
-    //   question: "Quelle méthode permet de retirer le dernier élément d'un tableau ?",
-    //   answers: ['shift()', 'pop()', 'splice()'],
-    //   correct: 'pop()',
-    // },
-    // {
-    //   question: 'Comment vérifier si une variable est un tableau en JavaScript ?',
-    //   answers: ['isType()', 'typeof', 'Array.isArray()'],
-    //   correct: 'Array.isArray()',
-    // },
-    // {
-    //   question:
-    //     'Quelle est la valeur de "null" lorsqu\'elle est convertie en nombre ?',
-    //   answers: ['0', '1', 'undefined', 'null'],
-    //   correct: '0',
-    // },
-    // {
-    //   question: 'Comment accéder au premier élément d\'un tableau appelé "myArray" ?',
-    //   answers: ['myArray[1]', 'myArray[0]', 'myArray.first()', 'myArray.one'],
-    //   correct: 'myArray[0]',
-    // },
+    {
+      question: "Comment appelle-t-on une fonction qui s'appelle elle-même ?",
+      answers: ['Fonction circulaire', 'Fonction récursive', 'Fonction miroir'],
+      correct: 'Fonction récursive',
+    },
+    {
+      question: 'Quelle fonction permet de créer un délai en JavaScript ?',
+      answers: ['delay()', 'setTimeout()', 'wait()'],
+      correct: 'setTimeout()',
+    },
+    {
+      question:
+        'Quel est le nom de la fonction qui permet de répéter une action à intervalles réguliers ?',
+      answers: ['setInterval()', 'repeat()', 'loop()'],
+      correct: 'setInterval()',
+    },
+    {
+      question: "Quelle méthode permet de retirer le dernier élément d'un tableau ?",
+      answers: ['shift()', 'pop()', 'splice()'],
+      correct: 'pop()',
+    },
+    {
+      question: 'Comment vérifier si une variable est un tableau en JavaScript ?',
+      answers: ['isType()', 'typeof', 'Array.isArray()'],
+      correct: 'Array.isArray()',
+    },
+    {
+      question:
+        'Quelle est la valeur de "null" lorsqu\'elle est convertie en nombre ?',
+      answers: ['0', '1', 'undefined', 'null'],
+      correct: '0',
+    },
+    {
+      question: 'Comment accéder au premier élément d\'un tableau appelé "myArray" ?',
+      answers: ['myArray[1]', 'myArray[0]', 'myArray.first()', 'myArray.one'],
+      correct: 'myArray[0]',
+    },
   ];
